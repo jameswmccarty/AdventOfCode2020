e8d8b78ba5bbb09f73bb059b5113d3a5d963f6ab
James McCarty <jameswmccarty@gmail.com>
2020-12-10 18:04:36 -0500

    Update Day 10 file (Part 2)

--- day10.py
+++ day10.py
@@ -146,6 +146,7 @@ What is the total number of distinct ways you can arrange the adapters to connec
 
 """
 
+
 if __name__ == "__main__":
 
 	adapters = []
@@ -169,3 +170,41 @@ if __name__ == "__main__":
 	print(ones*threes)
 
 	# Part 2 Solution
+	
+	# Explanation of the method:
+	# In example solution, the answer is 19208.
+	# Factor tree of 19208 is 2*2*2*7*7*7
+	# 
+	# The number of available paths from each link
+	# in the adapter chain, counting up from (0) to (22) is:
+	# 332113321113211211133211111332110
+	#
+	# Links with only 1 path are represented as 1
+	# links with multiple paths out form groups
+	# and the total number of paths is the product of these groups
+	#
+	# Links with multiple paths ordered by a single '2' double the number of solutions
+	# Links with a combination '32' yield 4 possible paths
+	# Links with a combination '332' yield 7 possible paths
+	# 
+	# Replacing these strings in the chain produce:
+	# 774277
+	#
+	# The product of these digits is our solution:
+	# 7*7*4*2*7 = 19208
+	
+	path = ""
+	for link in adapters:
+		paths = 0
+		if link + 1 in adapters:
+			paths += 1
+		if link + 2 in adapters:
+			paths += 1
+		if link + 3 in adapters:
+			paths += 1
+		path+=str(paths)
+	path = path.replace("332","7").replace("32","4").replace("1",'').replace("0",'')
+	total = 1
+	for digit in path:
+		total *= int(digit)
+	print(total)
