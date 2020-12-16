f2a3c895a9b99eeb81556dd96f6dea4a2c21d6a0
James McCarty <jameswmccarty@gmail.com>
2020-12-15 20:31:34 -0500

    Update Day 15 file (Part 2)

--- day15.py
+++ day15.py
@@ -74,3 +74,28 @@ if __name__ == "__main__":
 	print(start[-1])
 	
 	# Part 2 Solution
+	start = [16,12,1,0,15,7,11]
+	seen = set()
+	last = None
+	count = 0
+	nums = dict()
+
+	while len(start) > 1:
+		current = start.pop(0)
+		nums[current] = count
+		seen.add(current)
+		count += 1
+		last = current
+
+	last = start.pop(0)
+	while count < 30000000-1:
+		if last not in seen:
+			seen.add(last)
+			nums[last] = count
+			last = 0
+		else:
+			next = count - nums[last]
+			nums[last] = count
+			last = next
+		count += 1
+	print(last)
